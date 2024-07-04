960226cae94aab7c1d14e623a9410f6271f05c00
Chiaki Hayashi <chiaki0607@gmail.com>
2024-07-04 20:46:23 +0900

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "italian"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "chinese"}
+saizeriya =  {name: "Saizeriya", address: "Ebisu", category: "italian"}
+yoshinoya = {name: "Yoshinoya", address: "Meguro", category: "japanese"}
+kfc = {name: "KFC", address: "Shibuya", category: "belgian"}
+
+[dishoom, pizza_east, saizeriya, yoshinoya,kfc].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
